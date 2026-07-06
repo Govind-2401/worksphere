c56dc3b2c8fe341cde4d28edc1e13b293c8de02d
Govind Kumar <govindkumar.gpj@gmail.com>
2026-07-06 16:14:51 +0530

    Add Employee service layer

---
+++ src/main/java/com/govind/worksphere/service/impl/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.govind.worksphere.service;
+
+import com.govind.worksphere.entity.Employee;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+
+}
---
+++ src/main/java/com/govind/worksphere/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.govind.worksphere.service.impl;
+
+import com.govind.worksphere.entity.Employee;
+import com.govind.worksphere.repository.EmployeeRepository;
+import com.govind.worksphere.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
